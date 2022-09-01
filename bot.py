24418a0f0f96f9e345aac9c90de9d68dddc7dc81
Jigar Patel <jigar@JP-machine.local>
2022-09-01 13:32:23 -0400

    add tweet functionality

--- bot.py
+++ bot.py
@@ -1,5 +1,7 @@
 import requests
 import os
+from requests_oauthlib import OAuth1Session
+import json
 
 #retrieving API Ninja key from environment using os
 API_NINJA_KEY = os.environ.get("API_NINJA_KEY")
@@ -15,3 +17,40 @@ if response.status_code == requests.codes.ok:
 else:
     print("Error:", response.status_code, response.text)
 
+
+#Twitter automation portion starts here --
+
+# API_KEY and API_SECRET
+
+consumer_key = os.environ.get("CONSUMER_KEY")
+consumer_secret = os.environ.get("CONSUMER_SECRET")
+access_token = os.environ.get("ACCESS_TOKEN")
+access_token_secret = os.environ.get("ACCESS_TOKEN_SECRET")
+
+# Adding our fact to the payload to tweet
+payload = {"text": fact}
+
+# Make the request to twitter
+oauth = OAuth1Session(
+    consumer_key,
+    client_secret=consumer_secret,
+    resource_owner_key=access_token,
+    resource_owner_secret=access_token_secret,
+)
+
+# Making the request
+response = oauth.post(
+    "https://api.twitter.com/2/tweets",
+    json=payload,
+)
+
+if response.status_code != 201:
+    raise Exception(
+        "Request returned an error: {} {}".format(response.status_code, response.text)
+    )
+
+print("Response code: {}".format(response.status_code))
+
+# Saving the response as JSON
+json_response = response.json()
+print(json.dumps(json_response, indent=4, sort_keys=True))
