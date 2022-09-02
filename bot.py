48c3e76e3ae1d347c65bdecf18195edec46978f9
Jigar Patel <jigar@JP-machine.local>
2022-09-02 13:22:00 -0400

    code clean-up and add requirements file

--- bot.py
+++ bot.py
@@ -3,10 +3,14 @@ import json
 import requests
 import os
 
-#retrieving API Ninja key from environment using os
+#retrieving API keys and tokens from environment using os
 API_NINJA_KEY = os.environ.get("API_NINJA_KEY")
+consumer_key = os.environ.get("CONSUMER_KEY")
+consumer_secret = os.environ.get("CONSUMER_SECRET")
+access_token = os.environ.get("ACCESS_TOKEN")
+access_token_secret = os.environ.get("ACCESS_TOKEN_SECRET")
 
-#making an API call to retrieve a random fact
+#Retrieving a random fact
 limit = 1
 api_url = 'https://api.api-ninjas.com/v1/facts?limit={}'.format(limit)
 response = requests.get(api_url, headers={'X-Api-Key': API_NINJA_KEY})
@@ -14,18 +18,13 @@ response = requests.get(api_url, headers={'X-Api-Key': API_NINJA_KEY})
 if response.status_code == requests.codes.ok:
     fact = response.text
     x = len(fact)
+    # manipulating the response string to extract only a certain portion of the text.
     factpayload = fact[11:x-3]
 else:
     print("Error:", response.status_code, response.text)
 
 
 #Twitter automation portion starts here --
-
-# API_KEY and API_SECRET
-consumer_key = os.environ.get("CONSUMER_KEY")
-consumer_secret = os.environ.get("CONSUMER_SECRET")
-access_token = os.environ.get("ACCESS_TOKEN")
-access_token_secret = os.environ.get("ACCESS_TOKEN_SECRET")
 
 # Adding our fact to the payload to tweet
 payload = {"text": "⚡️ "+ factpayload+"."+" #Facts #MoreYouKnow"}
@@ -38,17 +37,16 @@ oauth = OAuth1Session(
     resource_owner_secret=access_token_secret,
 )
 
-# Making the request
+# Making the request to tweet
 response = oauth.post(
     "https://api.twitter.com/2/tweets",
     json=payload,
 )
-
+# throw an error if tweet creation was not success.
 if response.status_code != 201:
     raise Exception(
         "Request returned an error: {} {}".format(response.status_code, response.text)
     )
-
 print("Tweet success")
 
 # Saving the response as JSON
---
+++ requirements.py
@@ -0,0 +1,8 @@
+certifi==2022.6.15
+charset-normalizer==2.1.1
+idna==3.3
+oauthlib==3.2.0
+requests==2.28.1
+requests-oauthlib==1.3.1
+six @ file:///AppleInternal/BuildRoot/Library/Caches/com.apple.xbs/Sources/python3/python3-110/six-1.15.0-py2.py3-none-any.whl
+urllib3==1.26.12
